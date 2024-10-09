3b5d584946cf14e2b6c54adcee5f8ee29108c74e
Julio Cesar de Jesus <julio.c.jesus@hotmail.com>
2024-10-09 18:06:37 -0300

    feat: add dependency injecton mediator

---
+++ src/MoviesRental.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoviesRental.Application.Services.Dvds.Commands.CreateDvd;
+using System;
+using System.Reflection;
+
+namespace MoviesRental.Infra.IoC;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        //var connectionString = configuration.GetConnectionString("PostgressConexao");
+        //var connectionStringBuilder = new NpgsqlConnectionStringBuilder(connectionString);
+        //var searchPaths = connectionStringBuilder.SearchPath?.Split(',');
+
+        //services.AddDbContext<AppDbContext>(options =>
+        //{
+        //    options.UseNpgsql(connectionString, b =>
+
+        //    {
+        //        b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName);
+
+        //        if (searchPaths is { Length: > 0 })
+        //        {
+        //            var mainSchema = searchPaths[0];
+        //            b.MigrationsHistoryTable(HistoryRepository.DefaultTableName, mainSchema);
+        //        }
+        //    });
+        //});
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateDvdCommand).Assembly));
+
+        return services;
+    }
+
+}

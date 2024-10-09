7dff1afc92277b9d71de4a24f10db6fbc89df47b
Julio Cesar de Jesus <julio.c.jesus@hotmail.com>
2024-10-09 18:05:57 -0300

    feat: add write director repository

--- src/MoviesRental.Domain/Interfaces/IDirector/IDirectorWriteRepository.cs
+++ src/MoviesRental.Domain/Interfaces/IDirector/IDirectorWriteRepository.cs
@@ -6,6 +6,6 @@ public interface IDirectorWriteRepository
     Task<bool> CreateDirectorAsync(Director director);
     Task<bool> UpdateDirectorAsync(Director director);
     Task<bool> DeleteDirectorAsync(Director director);
-    Task<Director> GetDirectorWithMoviesAsync(Guid id);
-    Task<Director> GetDirectorByIdAsync(Guid id);
+    Task<Director?> GetDirectorWithMoviesAsync(Guid id);
+    Task<Director?> GetDirectorByIdAsync(Guid id);
 }
--- src/MoviesRental.Domain/Interfaces/IDvd/IDvdWriteRepository.cs
+++ src/MoviesRental.Domain/Interfaces/IDvd/IDvdWriteRepository.cs
@@ -1,10 +1,10 @@
-﻿
-using MoviesRental.Domain.Entities.Write;
+﻿using MoviesRental.Domain.Entities.Write;
 
-namespace MoviesRental.Domain.Interfaces;
+namespace MoviesRental.Domain.Interfaces.IDvd;
 public interface IDvdWriteRepository
 {
     Task<bool> CreateDvdAsync(Dvd dvd);
     Task<bool> UpdateDvdAsync(Dvd dvd);
     Task<bool> DeleteDvdAsync(Dvd dvd);
+    Task<Dvd> GetDvdByIdAsync(Guid id);
 }

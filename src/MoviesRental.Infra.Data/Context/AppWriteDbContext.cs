77444193c0ffd268b405c402fb9acd5811f66b4b
Julio Cesar de Jesus <julio.c.jesus@hotmail.com>
2024-10-09 18:07:18 -0300

    feat: add write dbcontext

---
+++ src/MoviesRental.Infra.Data/Context/AppWriteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesRental.Domain.Entities.Write;
+using System;
+
+namespace MoviesRental.Infra.Data.Context;
+public class AppWriteDbContext : DbContext
+{
+    public AppWriteDbContext(DbContextOptions<AppWriteDbContext> options) : base(options)
+    { }
+
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Dvd> Dvds { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppWriteDbContext).Assembly);
+    }
+}

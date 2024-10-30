9123ef30f62a46b3d98b95fb82119651d1da0af0
Julio Cesar de Jesus <julio.c.jesus@hotmail.com>
2024-10-30 14:25:26 -0300

    feat: add directors repositories

--- src/MoviesRental.Domain/Interfaces/IDirector/IDirectorReadRepository.cs
+++ src/MoviesRental.Domain/Interfaces/IDirector/IDirectorReadRepository.cs
@@ -3,6 +3,9 @@
 namespace MoviesRental.Domain.Interfaces.IDirector;
 public interface IDirectorReadRepository
 {
-    Task<List<DirectorRead>> GetAllDirectorsAsync();
-    Task<DirectorRead> GetDirectorByIdAsync(Guid id);
+    Task<DirectorRead> GetDirectorByIdAsync(string id);
+    Task<DirectorRead?> GetDirectorByNameAsync(string name);
+    Task<DirectorRead> CreateDirectorAsync(DirectorRead directorRead);
+    Task<bool> UpdateDirectorAsync(DirectorRead directorRead);
+    Task<bool> DeleteDirectorAsync(string id);
 }
---
+++ src/MoviesRental.Infra.Data/Repositories/Read/DirectorReadRepository.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Driver;
+using MoviesRental.Domain.Entities.Read;
+using MoviesRental.Domain.Interfaces.IDirector;
+using MoviesRental.Infra.Data.Context;
+
+namespace MoviesRental.Infra.Data.Repositories.Read;
+public class DirectorReadRepository : IDirectorReadRepository
+{
+    private readonly AppReadDbContext _context;
+
+    public DirectorReadRepository(AppReadDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<DirectorRead> CreateDirectorAsync(DirectorRead directorRead)
+    {
+        await _context.Directors.InsertOneAsync(directorRead);
+
+        return directorRead;
+    }
+
+    public async Task<bool> DeleteDirectorAsync(string id)
+    {
+        var result = await _context.Directors.DeleteOneAsync(x => x.Id == id);
+
+        return result.IsAcknowledged && result.DeletedCount > 0;
+    }
+
+    public async Task<DirectorRead> GetDirectorByIdAsync(string id)
+    {
+        return await _context.Directors.Find(x => x.Id.Equals(id)).FirstOrDefaultAsync();
+    }
+
+    public async Task<DirectorRead?> GetDirectorByNameAsync(string name)
+    {
+        return await _context.Directors.Find(x => x.FullName.ToLower().Equals(name.ToLower())).FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> UpdateDirectorAsync(DirectorRead directorRead)
+    {
+        var result = await _context.Directors.ReplaceOneAsync(x => x.Id == directorRead.Id, directorRead);
+
+        return result.IsAcknowledged && result.ModifiedCount > 0;
+    }
+}

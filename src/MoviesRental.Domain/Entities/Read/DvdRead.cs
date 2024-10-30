a795e823464deab1af547888092bf0b650c8fcc6
Julio Cesar de Jesus <julio.c.jesus@hotmail.com>
2024-10-30 14:24:36 -0300

    feat; add dvd read

--- src/MoviesRental.Domain/Entities/Read/DvdRead.cs
+++ src/MoviesRental.Domain/Entities/Read/DvdRead.cs
@@ -2,12 +2,12 @@
 public class DvdRead
 {
     public string Id { get; set; }
-    public string Title { get; private set; }
-    public string Genre { get; private set; }
-    public DateTime Publisher { get; private set; }
-    public bool IsAvailable { get; private set; }
-    public int Copies { get; private set; }
-    public string DirectorId { get; private set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public DateTime Publisher { get; set; }
+    public bool IsAvailable { get; set; }
+    public int Copies { get; set; }
+    public string DirectorId { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public DateTime DeletedAt { get; set; }
